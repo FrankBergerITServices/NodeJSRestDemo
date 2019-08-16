299176a4d6960b42b58200fcc4b532886604b308
Frank Berger <Frank.Berger@fm-berger.de>
2019-08-16 14:10:18 +0200

    initial release

---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  hrPool: {
+    user: 'hr',
+    password: 'hrpw',
+    connectString: 'ec2-54-81-240-58.compute-1.amazonaws.com/B01',
+    poolMin: 10,
+    poolMax: 10,
+    poolIncrement: 0
+  }
+};
